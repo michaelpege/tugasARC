10f0eaf4fd57cb7caa0fccfd3bfba84c87c434cc
PG <mpg066@gmail.com>
2020-01-06 21:28:53 +0700

    initial commit

---
+++ precache-manifest.8dd4538b6e28fcd3b422d48e01a305a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59bef8ed840536223c0eab6f4589d402",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2239294d2608fe201c5e",
+    "url": "/static/css/main.ae4ddd14.chunk.css"
+  },
+  {
+    "revision": "2ee013251317092e8eee",
+    "url": "/static/js/2.13e70b46.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.13e70b46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2239294d2608fe201c5e",
+    "url": "/static/js/main.c30d7c05.chunk.js"
+  },
+  {
+    "revision": "d4bda0103567c798b726",
+    "url": "/static/js/runtime-main.c0637af9.js"
+  }
+]);
